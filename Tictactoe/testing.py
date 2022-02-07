01c374fa164960f2572703d780bb96b6559219af
Olli Lähde <47543970+runefullit@users.noreply.github.com>
2022-02-07 09:26:36 +0200

    Removing playground test file from TicTacToe. Adding initial course material of the task Heredity.

--- Tictactoe/testing.py
+++
@@ -1,9 +0,0 @@
-from tictactoe import *
-
-board = [[O,X,X],[O,X,X],[O,X,O]]
-print(utility(board))
-
-# actionset = actions(rboard)
-# print(actionset)
-
-# rrboard = result(rboard, actions(rboard).pop())
