6adda243d6bb91bd9ae6f47554eb986f0fb8f13a
Olli Lähde <olli.e.lahde@gmail.com>
2022-02-24 17:00:13 +0200

    Implemented rounding to N significant digits.

--- N-gram/N-gram.py
+++ N-gram/N-gram.py
@@ -16,16 +16,16 @@ def super_strip(input_string):
     # Getting rid of capitalization and splitting on word separating symbols.
     lower_strings = re.split("[-'\n ]", input_string.lower())
 
-    output_string = list()
+    words = list()
     for word in lower_strings:
         # Removing all non-alphabetic characters from each word
         alpha_word = "".join(symbol for symbol in word if symbol.isalpha())
 
         # Filtering out short words and stopwords
         if alpha_word not in STOPWORDS and len(alpha_word) >= 2:
-            output_string.append(alpha_word)
+            words.append(alpha_word)
 
-    return " ".join(output_string)
+    return words
 
 # def get_Ngrams(text, N, startstop=True):
 #     """Return characterwise n-gram of size N."""
@@ -56,7 +56,8 @@ def super_strip(input_string):
 
 def get_Ngrams(words, N):
     '''Return word-wise n-gram of size N'''
-    words = ['['].append(words).append(']')
+    words.insert(0, 'START')
+    words.append('END')
     nGrams = dict()
     for i in range(len(words) - (N-1)):
         seq = tuple(words[i:i+N])
@@ -67,35 +68,31 @@ def get_Ngrams(words, N):
     nGrams = {k: v/valsum for k, v in nGrams.items()} # Normalize to probabilities.
     return nGrams
 
-
-def proba(test_phrase, nGrams):
+def proba(test_phrase, nGrams, N):
     """Return the probability of the test phrase existing given learned nGrams"""
-    # Getting the length of the first element to pass info of what N is.
-    N = len(list(nGrams.keys())[0])
+    words = super_strip(test_phrase)
     prob = 1
-    for i in range(len(test_phrase) - (N-1)):
-        seq = test_phrase[i:i+N]
+    for i in range(len(words) - (N -1)):
+        seq = tuple(words[i:i+N])
         prob *= nGrams[seq]
     return prob
+
+def getNSignificantDigits(num, N):
+    """Return N significant digits without rounding."""
+    fstr = f"{num:#.{N}g}" # To string. Remove leading zeros, keep trailing.
+    fstr = "".join(ch for ch in fstr if ch.isnumeric()) # Remove any punctuation.
+    return  int(fstr)
+            
 
 def main():
     contents = filtered_read("TheStoryofAnHour-KateChopin.txt")
     test_phrase = 'no one'
 
-    nGrams = get_Ngrams(contents.split(), 2)
-    print(nGrams[:10])
-    
-    # # With start and stop markers added
-    # nGrams = get_Ngrams(contents, 2)
-    # prob = proba(test_phrase, nGrams)
-    # rem = int("".join(str(prob)[:4].split('.'))) % 173
-    # print(f"Remainder with start and stop markers: {rem}")
-
-    # # Without start and stop markers
-    # nGrams = get_Ngrams(contents, 2, startstop=False)
-    # prob = proba(test_phrase, nGrams)
-    # rem = int("".join(str(prob)[:4].split('.')))%173
-    # print(f"Remainder without start and stop markers: {rem}")
+    nGrams = get_Ngrams(contents, 2)
+    prob = proba(test_phrase, nGrams, 2)
+    significants = getNSignificantDigits(prob, 3)
+    rem = significants%173
+    print(f"Remainder without start and stop markers: {rem}")
 
 
 if __name__ == "__main__":
